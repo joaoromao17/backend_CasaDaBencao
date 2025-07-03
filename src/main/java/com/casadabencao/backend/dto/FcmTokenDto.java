16f86859d4875b0c0f2771ce33cc887929017aa8
joaoromao17 <101903500+joaoromao17@users.noreply.github.com>
2025-07-03 14:52:22 -0300

    Create FcmTokenDto.java

---
+++ src/main/java/com/casadabencao/backend/dto/FcmTokenDto.java
@@ -0,0 +1,13 @@
+package com.casadabencao.backend.dto;
+
+public class FcmTokenDto {
+    private String fcmToken;
+
+    public String getFcmToken() {
+        return fcmToken;
+    }
+
+    public void setFcmToken(String fcmToken) {
+        this.fcmToken = fcmToken;
+    }
+}
