fd4c8df50810842893c1d4205cedd6ccbde2329e
joaoromao17 <101903500+joaoromao17@users.noreply.github.com>
2025-07-03 13:18:02 -0300

    Update WebConfig.java

--- src/main/java/com/casadabencao/backend/config/WebConfig.java
+++ src/main/java/com/casadabencao/backend/config/WebConfig.java
@@ -5,7 +5,7 @@ import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-import javax.annotation.PostConstruct;
+import jakarta.annotation.PostConstruct;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
